78e28f8ebda668fb4b40fc273f5206b213273d0b
Ivan Krister Garcia <kristernavi@gmail.com>
2018-03-06 12:12:42 +0800

    added config for new database

--- app/Card.php
+++ app/Card.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
+    protected $connection = 'mysql2';
+
     protected function enoughBalance($paidAmount)
     {
         $remain = $this->balance - $paidAmount;
